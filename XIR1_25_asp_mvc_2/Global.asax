d1fd0ac8217e73355160d85fcaab2515f20771e9
Ocha Putri Perdana Prihatina <ocha_prihatina_24rpl@student.smktelkom-mlg.sch.id>
2017-01-19 16:20:32 +0700

    Initial Commit

---
+++ XIR1_25_asp_mvc_2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="XIR1_25_asp_mvc_2.MvcApplication" Language="C#" %>
---
+++ XIR1_25_asp_mvc_2/Models/ViewModel/UserModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace XIR1_25_asp_mvc_2.Models.ViewModel
+{
+    public class UserModel
+    {
+    }
+}
